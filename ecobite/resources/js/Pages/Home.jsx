5515b0a3dfb4ee775564a9e3831a1292ac2de1ec
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-11-25 17:33:59 +0100

    Set up React and Inertia

---
+++ ecobite/resources/js/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome to EcoBite</h1>
+            <p>Your go-to app for sustainable living.</p>
+        </div>
+    );
+};
