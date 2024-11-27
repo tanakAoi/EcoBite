9c0a223476e8dadc235727678c57e8a2e637aed4
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-11-27 13:20:21 +0100

    Install Laravel Breeze

---
+++ routes/auth.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordController;
+
+
+
