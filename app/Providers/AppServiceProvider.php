0b2798dcfaf8da91e6505881fd634f81fee357db
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-11-29 19:58:03 +0100

    Update AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if ($this->app->environment('production') || $this->app->environment('dev')) {
+            URL::forceScheme('https');
+        }
     }
 
     /**
