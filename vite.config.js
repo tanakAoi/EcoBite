3913a0efaa142179334ad138d95370e58fe22805
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-11-29 18:12:47 +0100

    Modify vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/ts/app.tsx',
+            input: "resources/ts/app.tsx",
             refresh: true,
         }),
         react(),
     ],
+    build: {
+        outDir: "public/build",
+    },
 });
