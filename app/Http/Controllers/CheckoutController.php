dab5d52f890d87faafe84e048c38c6f5289522e2
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-12-16 09:55:11 +0100

    Fix url

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -35,7 +35,8 @@ class CheckoutController extends Controller
             'currency' => 'sek',
             'automatic_payment_methods' => ['enabled' => true],
         ]);
-        $returnUrl = config('app.url') . '/checkout/order-confirmation';
+
+        $returnUrl = config('app.url') . 'checkout/order-confirmation';
 
         return response()->json([
             'client_secret' => $intent->client_secret, 
